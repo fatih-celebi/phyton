f826d76bf13c2b5cc35591f763ed2ef50d043c5b
fatih-celebi <celebifatih.c1@gmail.com>
2023-07-07 23:44:05 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,14 @@
+
+
+string = "bursa"
+def swap_the_case_of_a_string(string):
+    return ''.join(reversed(string))
+
+print(swap_the_case_of_a_string(string))
+
+
+
+def is_integer_odd(integer):
+    return str(integer)
+
+print(is_integer_odd(6))
